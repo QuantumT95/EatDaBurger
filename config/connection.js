d254f2c15a075bf86a260aa468f45eefc7a3c467
QuantumT95 <tran.charlie@ymail.com>
2017-11-07 18:31:20 -0600

    ftrying to get db to work in heroku

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,16 @@
 var mysql = require("mysql");
 
-// if (process.env.JAWSDB_URL) {
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// }
-// else {
-//     connection = mysql.createConnection({
-//         host: "vhw3t8e71xdz9k14.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-//         user: "o0ee8xcj6fbigv53",
-//         password: "lvgb8pt3wzxzml06",
-//         port: 3306,
-//         database: "burgers_db"
-//     });
-// }
-
-connection = mysql.createConnection({
-    host: "vhw3t8e71xdz9k14.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "o0ee8xcj6fbigv53",
-    password: "lvgb8pt3wzxzml06",
-    port: 3306,
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+}
 
 connection.connect(function (err) {
     if (err) throw err;
